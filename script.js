a669a658585ebdfaef3b5e8732f263412eb2b60d
Camilla Aoki <caokicontato@gmail.com>
2024-02-15 23:31:02 -0300

    adicinei algumas melhorias no menu categorias quando estiver em uma tela maior

--- script.js
+++ script.js
@@ -11,5 +11,19 @@ const swiper = new Swiper('.swiper', {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
-  
+
+});
+
+document.addEventListener('click', function(event) {
+  var checkbox = document.querySelector('.opcoes__botao');
+  var label = document.querySelector('.opcoes__label');
+
+  // Verifica se o clique foi dentro ou fora da caixa de opções
+  if (label.contains(event.target) || checkbox.contains(event.target)) {
+    // Clique dentro da caixa de opções ou no próprio checkbox, marca/desmarca o checkbox
+    checkbox.checked = !checkbox.checked;
+  } else {
+    // Clique fora da caixa de opções e do checkbox, desmarca o checkbox
+    checkbox.checked = false;
+  }
 });
